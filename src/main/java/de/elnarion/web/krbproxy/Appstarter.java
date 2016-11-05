b1c7bd9d6d07b7d0f116c09c2a3abdd07249a8fd
lauermat <dev.lauer@gmail.com>
2016-11-05 20:50:19 +0100

    added new parameters for special negotiate handling

--- src/main/java/de/elnarion/web/krbproxy/Appstarter.java
+++ src/main/java/de/elnarion/web/krbproxy/Appstarter.java
@@ -14,8 +14,24 @@ public class Appstarter {
 	}
 
 	public static void main(String[] args) throws Exception {
+		
 		Server server = new Server(Integer.parseInt(args[0]));
-		 
+		
+	      
+	      String negotiateURL = args[3];
+	      String negotiateSPN = args[4];
+		
+
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.print("Enter username");
+//        String username = br.readLine();
+//        System.out.print("Enter password");
+//        String password = br.readLine();
+//        System.out.print("Enter domain");
+//        String domain = br.readLine();
+//        System.out.print("Enter workstation");
+//        String workstation = br.readLine();
+        
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
@@ -23,7 +39,16 @@ public class Appstarter {
         ServletHolder holder = new ServletHolder(new ProxyServlet());
         HashMap<String, String> map = new HashMap<String, String>();
         map.put("targetUri", args[1]);
+        map.put("proxyURL", "http://localhost:"+args[0]);
         map.put("log", "true");
+//        map.put("user", username);
+//        map.put("password", password);
+//        map.put("domain", domain);
+//        map.put("host", workstation);
+        map.put("negotiate.refreshtime", "100000");
+        map.put("negotiate.url", negotiateURL);
+        map.put("negotiate.spn", negotiateSPN);
+
         holder.setInitParameters(map);
         context.addServlet(holder,args[2]);
  
