9b060b065a133e6282e789188a9832ae9a69c607
dev.lauer <dev.lauer@elnarion.de>
2017-05-03 22:32:18 +0200

    changed parameters

--- src/main/java/de/elnarion/web/krbproxy/Appstarter.java
+++ src/main/java/de/elnarion/web/krbproxy/Appstarter.java
@@ -9,51 +9,39 @@ import org.mitre.dsmiley.httpproxy.ProxyServlet;
 
 public class Appstarter {
 
-
 	public Appstarter() {
 	}
 
 	public static void main(String[] args) throws Exception {
-		
+
 		Server server = new Server(Integer.parseInt(args[0]));
-		
-	      
-	      String negotiateURL = args[3];
-	      String negotiateSPN = args[4];
-		
 
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        System.out.print("Enter username");
-//        String username = br.readLine();
-//        System.out.print("Enter password");
-//        String password = br.readLine();
-//        System.out.print("Enter domain");
-//        String domain = br.readLine();
-//        System.out.print("Enter workstation");
-//        String workstation = br.readLine();
-        
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
- 
-        ServletHolder holder = new ServletHolder(new ProxyServlet());
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put("targetUri", args[1]);
-        map.put("proxyURL", "http://localhost:"+args[0]);
-        map.put("log", "true");
-//        map.put("user", username);
-//        map.put("password", password);
-//        map.put("domain", domain);
-//        map.put("host", workstation);
-        map.put("negotiate.refreshtime", "100000");
-        map.put("negotiate.url", negotiateURL);
-        map.put("negotiate.spn", negotiateSPN);
+		String negotiateURL = null;
+		String negotiateSPN = null;
+		if (args.length > 3) {
+			negotiateURL = args[3];
+			negotiateSPN = args[4];
+		}
 
-        holder.setInitParameters(map);
-        context.addServlet(holder,args[2]);
- 
-        server.start();
-        server.join();
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+
+		ServletHolder holder = new ServletHolder(new ProxyServlet());
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("targetUri", args[1]);
+		map.put("proxyURL", "http://localhost:" + args[0]);
+		map.put("log", "false");
+		map.put("negotiate.refreshtime", "100000");
+		if (negotiateURL != null) {
+			map.put("negotiate.url", negotiateURL);
+			map.put("negotiate.spn", negotiateSPN);
+		}
+		holder.setInitParameters(map);
+		context.addServlet(holder, args[2]);
+
+		server.start();
+		server.join();
 	}
 
 }
