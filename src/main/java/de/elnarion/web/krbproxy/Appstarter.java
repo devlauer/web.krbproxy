bc59b66ae8441d847495e2874a21e5ddc7a5122b
dev.lauer <dev.lauer@elnarion.de>
2017-11-01 17:56:40 +0100

    changed encoding; updated to java 8; added javadoc

--- src/main/java/de/elnarion/web/krbproxy/Appstarter.java
+++ src/main/java/de/elnarion/web/krbproxy/Appstarter.java
@@ -7,11 +7,32 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.mitre.dsmiley.httpproxy.ProxyServlet;
 
+/**
+ * Starter class for the kerberos/NTLM proxy, which uses the local kerberos TGT
+ * or windows NTLM to access a remote address. This is useful if a program can
+ * not access this remote address because it is not aware of the kerberos/NTLM
+ * negotiate.
+ * 
+ * @author dev.lauer@elnarion.de
+ *
+ */
 public class Appstarter {
 
+	/**
+	 * Default constructor
+	 */
 	public Appstarter() {
 	}
 
+	/**
+	 * Starts up the kerberos proxy.
+	 * 
+	 * @param args
+	 *            the local port for the proxy (0), the remote URL for the proxy
+	 *            (1), the local context root (2), the negotiate URL of the remote
+	 *            server (3), the negotiate SPN of the remote server (4)
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 
 		Server server = new Server(Integer.parseInt(args[0]));
